af4c79bcebb1bf12e326d10ff1f73a91a003865e
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-10-02 12:29:10 -0500

    refactor: Se generan las rutas para la navegación entre páginas dinámicamente.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,10 +13,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        print('la ruta se llama: ${settings.name}');
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
