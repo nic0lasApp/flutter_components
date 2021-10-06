e590e355a27aa7afa01813368a6066c91d19b085
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-10-06 17:15:04 -0500

    feat: Se agrega modal de alerta con botones para aceptar y cancelar.

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,16 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('mostrar alerta'),
+          onPressed: () => _mostrarAlert(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.blue,
+              onPrimary: Colors.white,
+              shape: StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.logout),
         onPressed: () {
@@ -17,4 +27,44 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void _mostrarAlert(BuildContext context) {
+  showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Titulo'),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido'),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text('cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                onPrimary: Colors.blue,
+              ),
+            ),
+            ElevatedButton(
+              child: Text('ok'),
+              onPressed: () => Navigator.of(context).pop(),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                onPrimary: Colors.blue,
+              ),
+            ),
+          ],
+        );
+      });
 }
