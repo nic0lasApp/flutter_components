1f1abec97f871c4170dd2d2239527aa6afa244a5
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-10-07 14:44:23 -0500

    feat: Se implementa avatar page.

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
@@ -8,6 +10,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          CircleAvatar(
+            backgroundImage:
+                NetworkImage('https://picsum.photos/id/237/200/300'),
+            radius: 25.0,
+          ),
+          // ignore: avoid_unnecessary_containers
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(right: 10.0),
+              child: CircleAvatar(
+                child: Text('SL'),
+                backgroundColor: Colors.brown,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://picsum.photos/id/237/200/300'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
