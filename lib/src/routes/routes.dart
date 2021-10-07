b82439743286e023029dab20f9d1ced9082bbe89
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-10-07 16:04:31 -0500

    feat: Se implementa página para container y animated container.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:componentes/src/pages/card_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/card_pages.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -12,5 +13,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
   };
 }
