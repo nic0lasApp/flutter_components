0bafa2dbd846613ec235d37fcde1c21bb578b8bb
Nicolas_Aponte <sistemas3@arios-ing.com>
2021-10-08 00:30:52 -0500

    feat: Se crea página para manejo de inputfields.

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Input de texto'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: [
+          _crearInput(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Nombre',
+        helperText: 'Solo es el nombre',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (valor) {
+        _nombre = valor;
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      title: Text('nombre es: $_nombre'),
+    );
+  }
+}
